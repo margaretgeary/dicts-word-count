6d1f135f90853430a0b28bdecb98c91b2f87b8bf
Margaret Geary <gearymarge@gmail.com>
2020-07-25 23:56:58 +0000

    Final commit

---
+++ wordcount.py
@@ -0,0 +1,20 @@
+# put your code here.
+
+def word_count(txt_file):
+
+    lines = open(txt_file)
+
+    word_counts = {}
+
+    for line in lines:
+        word = line.split()
+    
+        for item in word:
+            if item in word_counts:
+                word_counts[item] += 1
+            else:
+                word_counts[item] = word_counts.get(item, 0) + 1
+    print(word_counts)
+    return word_counts
+
+word_count("test.txt")
